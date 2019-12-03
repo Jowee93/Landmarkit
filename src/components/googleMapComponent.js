e4d0a0c58d357330f7620653b879ebccdac9a34b
Jowee93 <yeapjowee@gmail.com>
2019-12-03 13:59:12 +0800

    Change googlemap parameters

--- src/components/googleMapComponent.js
+++ src/components/googleMapComponent.js
@@ -55,7 +55,7 @@ class GoogleMapComponent extends React.Component {
     const parameters = {
       section: "outdoors",
       // near: "Penang",
-      ll: "3.1461,101.6255",
+      ll: this.props.position.latitude + "," + this.props.position.longitude,
       limit: 3,
       radius: 5000,
       client_secret: process.env.REACT_APP_FOURSQUARE_CLIENT_SECRET,
@@ -80,106 +80,110 @@ class GoogleMapComponent extends React.Component {
           Response.data.response.groups[0].items[0].venue.categories[0].icon
             .prefix +
             Response.data.response.groups[0].items[0].venue.categories[0].icon
-              .suffix 
+              .suffix
         );
       })
       .catch(error => {
         console.log("ERROR! " + error);
       });
 
-      // axios.post(
-      //   `https://api.foursquare.com/v2/venues/explore?` +
-      //     new URLSearchParams(parameters)
-      // )
-      // .then(response => {
-      //   console.log("infos", response.data.response.groups[0].items[0].reasons)
-      // })
-    console.log(this.props.position);
+    // axios.post(
+    //   `https://api.foursquare.com/v2/venues/explore?` +
+    //     new URLSearchParams(parameters)
+    // )
+    // .then(response => {
+    //   console.log("infos", response.data.response.groups[0].items[0].reasons)
+    // })
+    console.log(this.props.position.latitude);
   }
 
   render() {
     return (
       <>
-      <Container className="d-flex justify-content-center">
-        <div
-          style={{
-            position: "relative",
-            width: "100vw",
-            height: "50vh"
-          }}
-        >
-          <Map
-            google={this.props.google}
-            zoom={16}
-            style={mapStyles}
-            initialCenter={{ lat:3.1461, lng:101.6255}}
+        <Container className="d-flex justify-content-center">
+          <div
+            style={{
+              position: "relative",
+              width: "100vw",
+              height: "50vh"
+            }}
           >
-            {this.state.places.map((place, index) => {
-              return (
-                <Marker
-                  key={index}
-                  onClick={this.onMarkerClick}
-                  position={{
-                    lat: place.venue.location.lat,
-                    lng: place.venue.location.lng
-                  }}
-                  name={place.venue.name}
-                />
-              );
-            })}
-
-            <InfoWindow
-              marker={this.state.activeMarker}
-              visible={this.state.showingInfoWindow}
-              onClose={this.onClose}
+            <Map
+              google={this.props.google}
+              zoom={15}
+              style={mapStyles}
+              initialCenter={{
+                lat: this.props.position.latitude,
+                lng: this.props.position.longitude
+              }}
             >
-              <div>
-                <h4>
-                  {this.state.selectedPlace && this.state.selectedPlace.name}
-                </h4>
-              </div>
-            </InfoWindow>
-          </Map>
-          {/* <div style={{position: 'absolute', top: '100%'}}>
+              {this.state.places.map((place, index) => {
+                return (
+                  <Marker
+                    key={index}
+                    onClick={this.onMarkerClick}
+                    position={{
+                      lat: place.venue.location.lat,
+                      lng: place.venue.location.lng
+                    }}
+                    name={place.venue.name}
+                  />
+                );
+              })}
+
+              <InfoWindow
+                marker={this.state.activeMarker}
+                visible={this.state.showingInfoWindow}
+                onClose={this.onClose}
+              >
+                <div>
+                  <h4>
+                    {this.state.selectedPlace && this.state.selectedPlace.name}
+                  </h4>
+                </div>
+              </InfoWindow>
+            </Map>
+            {/* <div style={{position: 'absolute', top: '100%'}}>
           <div style={{ position: "absolute", top: "100%" }}>
             {this.state.activeMarker && this.state.activeMarker.name}
           </div> */}
           </div>
-          </Container>
+        </Container>
 
-          {this.state.places.map(place => {
-            if(this.state.activeMarker && (place.venue.name === this.state.activeMarker.name)){
-              return (
-                <div
-                  // style={{display : this.state.activeMarker.name == place.venue.name ? "" : "hidden"}}
-                  className="card"
-                >
-                  <div className="container">
-                    <h4>{place.venue.name}</h4>
-                    {/* <img
+        {this.state.places.map(place => {
+          if (
+            this.state.activeMarker &&
+            place.venue.name === this.state.activeMarker.name
+          ) {
+            return (
+              <div
+                // style={{display : this.state.activeMarker.name == place.venue.name ? "" : "hidden"}}
+                className="card"
+              >
+                <div className="container">
+                  <h4>{place.venue.name}</h4>
+                  {/* <img
                         src={
                           place.venue.categories[0].icon.prefix +
                           88 +
                           place.venue.categories[0].icon.suffix
                         }
                       /> */}
-                    <span>
-                      {place.venue.location.address}{" "}
-                      {place.venue.location.crossStreet},{" "}
-                      {place.venue.location.postalCode},{" "}
-                      {place.venue.location.city}{" "}
-                      {place.venue.location.street}{" "}
-                      {place.venue.location.country}{" "}
-                    </span>
-                    <div className="innerContainer">
-                      {place.venue.categories[0].pluralName}
-                    </div>
+                  <span>
+                    {place.venue.location.address}{" "}
+                    {place.venue.location.crossStreet},{" "}
+                    {place.venue.location.postalCode},{" "}
+                    {place.venue.location.city} {place.venue.location.street}{" "}
+                    {place.venue.location.country}{" "}
+                  </span>
+                  <div className="innerContainer">
+                    {place.venue.categories[0].pluralName}
                   </div>
                 </div>
-              );
-
-            }
-          })}
+              </div>
+            );
+          }
+        })}
       </>
     );
   }
